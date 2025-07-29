05d1a6756b40dd91b6da0a40702a79bc0f622fa7
ابو يحيى عبدالله <abdalla-yahia>
2025-07-29 20:47:44 +0300

    Edit Marqeu

--- src/components/Home/BrandsCarousel-Home.tsx
+++ src/components/Home/BrandsCarousel-Home.tsx
@@ -2,29 +2,40 @@ import Image from "next/image";
 import Marquee from "react-fast-marquee";
 
 const brands = [
-  "/brands/brand1.png",
-  "/brands/brand2.png",
-  "/brands/brand3.png",
-  "/brands/brand4.png",
-  "/brands/brand5.png",
-  "/brands/brand6.png",
-  "/brands/brand1.png",
-  "/brands/brand2.png",
-  "/brands/brand3.png",
-  "/brands/brand4.png",
-  "/brands/brand5.png",
-  "/brands/brand6.png",
-  "/brands/brand1.png",
-  "/brands/brand2.png",
-  "/brands/brand3.png",
-  "/brands/brand4.png",
-  "/brands/brand5.png",
-  "/brands/brand6.png",
+  "https://www.logo.wine/a/logo/Adidas/Adidas-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Nike,_Inc./Nike,_Inc.-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Louis_Vuitton/Louis_Vuitton-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Gucci/Gucci-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Chanel/Chanel-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Under_Armour/Under_Armour-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Dolce_%26_Gabbana/Dolce_%26_Gabbana-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/L%C3%ADder/L%C3%ADder-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Hugo_Boss/Hugo_Boss-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Gul_(watersports)/Gul_(watersports)-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Puma_(brand)/Puma_(brand)-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Slazenger/Slazenger-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/The_Trump_Organization/The_Trump_Organization-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Ralph_Lauren_Corporation/Ralph_Lauren_Corporation-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Luxottica/Luxottica-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/New_Balance/New_Balance-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Reebok/Reebok-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Christian_Dior_(fashion_house)/Christian_Dior_(fashion_house)-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Converse_(shoe_company)/Converse_(shoe_company)-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Armani/Armani-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Fendi/Fendi-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Columbia_Sportswear/Columbia_Sportswear-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Boots_(company)/Boots_(company)-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Canterbury_of_New_Zealand/Canterbury_of_New_Zealand-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Samsung_C%26T_Corporation/Samsung_C%26T_Corporation-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Joma/Joma-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Lyle_%26_Scott/Lyle_%26_Scott-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Toyobo/Toyobo-Logo.wine.svg",
+  "https://www.logo.wine/a/logo/Fenchurch_(clothing)/Fenchurch_(clothing)-Logo.wine.svg",
 ];
 
 export default function BrandCarousel() {
   return (
-    // <div className="w-full overflow-hidden py-6 bg-white border-y border-gray-200">
+    <div className="py-1 bg-white border-y border-gray-200">
           <Marquee autoFill={true} >
 
           {brands.map((src, i) => (
@@ -40,6 +51,6 @@ export default function BrandCarousel() {
           ))}
           </Marquee>
 
-            // </div>
+          </div>
   );
 }
