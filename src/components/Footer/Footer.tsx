32c67be2ec3e1aa7f0bed830668c5cb534bdb297
ابو يحيى عبدالله <abdalla-yahia>
2025-07-30 00:32:13 +0300

    Edit Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,19 +2,19 @@ import Link from "next/link";
 import * as icon from '@/utils/Icons/Icons'
 export default function Footer() {
   return (
-        <div className="w-full flex justify-evenly items-center bg-accent text-foreground p-1 rounded" >
+        <div className="w-full flex flex-col justify-center items-center bg-accent text-foreground p-1 rounded" >
             <div>
             <hr className="my-0 border-white  sm:mx-auto z-0 lg:my-8" />
-                    <span className="flex justify-center items-center md:block max-w-full text-sm text-text_color sm:text-center">جميع الحقوق محفوظة  ©  {new Date().getFullYear()} <Link href="mailto:abdalla_y2007@yahoo.com/" className="hover:underline text-orange-600 font-bold text-lg">موقع طالب علم™ </Link> </span>
+                    <span className="flex justify-center items-center md:block max-w-full text-sm text-text_color sm:text-center">جميع الحقوق محفوظة  ©  {new Date().getFullYear()} <Link href="mailto:abdalla_y2007@yahoo.com/" className="hover:underline text-orange-600 font-bold text-lg">متجر اسواق ™ </Link> </span>
                   </div><br></br>
                   {/**Site Owner */}
-                          <div className=" max-w-full flex justify-center items-center md:block lg:block  text-sm text-gray-200 text-center">تم تصميم وتطوير الموقع بواسطة  
+                          <div className=" max-w-full flex justify-center items-center md:block lg:block  text-sm text-muted text-center">تم تصميم وتطوير الموقع بواسطة  
                             <Link href="https://www.linkedin.com/in/abdalla-yahia/" target="_blank" className="hover:underline text-blue-700 font-bold text-lg flex justify-center items-center">م / عبدالله يحيى 
                               <span className="text-red-600">™</span>
-                              <span className=" max-w-full text-sm text-gray-200 sm:text-center">جميع الحقوق محفوظة  ©</span>
+                              <span className=" max-w-full text-sm text-muted sm:text-center">جميع الحقوق محفوظة  ©</span>
                                 </Link><span> للتواصل: 
                                   <icon.FaWhatsapp className="text-green-700 inline-flex mx-2"/>
-                                  <Link href="https://wa.me/201211100554?text=مرحباً%مهندس/%20!!%عبدالله%أرغب%في%20التواصل%20معكم%20لتطوير%20موقع" target="_blank">01211100554</Link></span> 
+                                  <Link href="https://wa.me/201211100554?text=مرحباً%مهندس/%20!!%عبدالله%أرغب%في%20التواصل%20معكم%20لتطوير%20موقع" target="_blank" className="text-muted">01211100554</Link></span> 
                           </div>
                   <br></br>
         </div>
