4a6ad294e27546463006338b3464d0c4d39d3a5b
ابو يحيى عبدالله <abdalla-yahia>
2025-08-04 21:26:07 +0300

    Create Slider In Landing Page

--- src/app/admins/layout.tsx
+++ src/app/admins/layout.tsx
@@ -2,11 +2,11 @@ import Admins_Dashboard from '@/components/Admins/Admins_Dashboard';
 import MainTitle from '@/utils/Main-Title';
 
 export default function AdminsLayout({ children }: { children: React.ReactNode }) {
-  const name ="Ali"
+  const name ="عبدالله يحيى"
   return (
         <div className="mt-[180px] mx-5 flex flex-col justify-start items-start">
             {/**Title Page */}
-            <MainTitle title={`لوحة تحكم المسؤل  ${name}`}/>
+            <MainTitle title={`لوحة تحكم المسؤول  ${name}`}/>
             <div className='w-full flex justify-start items-start gap-1'>
               {/**Aside Menu */}
               <div className="w-1/8 md:w-1/6 lg:w-1/6 flex justify-start items-start gap-3 my-3">
