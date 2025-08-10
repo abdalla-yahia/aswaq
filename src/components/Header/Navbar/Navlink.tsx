01dde8a3294633a7445dbda6d7ae2c0d01e95525
ابو يحيى عبدالله <abdalla-yahia>
2025-08-10 23:59:40 +0300

    Add Setting Icon On Header

--- src/components/Header/Navbar/Navlink.tsx
+++ src/components/Header/Navbar/Navlink.tsx
@@ -13,12 +13,16 @@ console.log(data?.me?.image)
   return (
     <>
       <ul className="flex w-full md:w-1/3 order-3 md:order-1 mb-2 md:mb-0 justify-center items-start gap-3 md:gap-2 text-lg">
-        <Link href={'/cart'} className=" relative">
+        <Link href={'/cart'} className=" relative flex flex-col justify-center items-center text-center">
           <li >العربة</li>
           <icon.FaOpencart />
-          <span className="absolute top-5 -right-6 p-1 text-xs rounded-2xl bg-red-800 text-white">5</span>
+          <span className="absolute top-[50%] -right-3 flex size-5">
+            <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+            <span className="relative inline-flex size-5 rounded-full bg-sky-500 justify-center items-center text-white text-[12px]">15</span>
+          </span>
+          {/* <span className="absolute top-0 -right-6 p-1 text-xs animate-ping rounded-2xl bg-red-800 text-white">5</span> */}
         </Link>
-        <span className="text-[10px] text-orange-600 font-bold">2500 ج.م</span>
+        {/* <span className="text-[10px] text-orange-600 font-bold">2500 ج.م</span> */}
         <Link href={'/login'} className="flex flex-col justify-center items-center text-center">
           <li >{data ? (`مرحباً ${(data?.me?.name).split(' ')[0]}`):'دخول'}</li>
           {
