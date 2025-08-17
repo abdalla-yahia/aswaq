f8e0b53403ee64d0e3e5c7931e438351086d2488
ابو يحيى عبدالله <abdalla-yahia>
2025-08-17 23:54:06 +0300

    Finish Category Actions

--- src/validations/CategoryValidation.ts
+++ src/validations/CategoryValidation.ts
@@ -2,7 +2,7 @@ import {z} from 'zod';
 
 export const CreateCategoryValidation = z.object({
     name:z.string().min(3,'اسم التصنيف لا يقل عن ثلاثة احرف').max(100),
-    description:z.string().min(20).max(300).optional(),
+    description:z.string().max(300).optional().nullable(),
     image:z.string().optional().nullable(),
     parentId:z.string().optional().nullable()
 })
@@ -10,7 +10,7 @@ export const CreateCategoryValidation = z.object({
 export const UpdateCategoryValidation = z.object({
     id:z.string().optional(),
     name:z.string().min(3,'اسم التصنيف لا يقل عن ثلاثة احرف').max(100),
-    description:z.string().min(20).max(300).optional(),
+    description:z.string().max(300).optional().nullable(),
     image:z.string().optional().nullable(),
     parentId:z.string().optional().nullable()
 })
